423fe50d0f77366c9889c8e9aa25e66c7394aa71
PanupongASsystem <PanupongI@assystem.co.th>
2024-05-27 14:37:45 +0700

    new

--- application/views/frontend/guide_work.php
+++ application/views/frontend/guide_work.php
@@ -1,22 +1,13 @@
-<div class="bg-pages-news">
+<div class="text-center" style="padding-top: 65px">
+    <span class="font-pages-head">คู่มือการปฏิบัติงาน</span>
+</div>
+</div>
+
+<div class="bg-pages">
     <div class="container-pages-news">
-        <div class="page-center">
-            <div class="head-pages">
-                <span class="font-pages-head">คู่มือการปฏิบัติงาน</span>
-            </div>
-        </div>
-        <div class="row">
-            <div class="path1-1">
-                <span class="font-path-1 underline"><a href="<?php echo site_url('Home'); ?>">หน้าแรก</a></span>
-            </div>
-            <div class="path2-4">
-                <span class="font-path-2 underline"><a href="#">คู่มือการปฏิบัติงาน</a></span>
-            </div>
-        </div>
-        <div style="padding-top: 40px;"></div>
         <?php
         $count = count($query);
-        $itemsPerPage = 10; // จำนวนรายการต่อหน้า
+        $itemsPerPage = 27; // จำนวนรายการต่อหน้า
         $totalPages = ceil($count / $itemsPerPage);
 
         $currentPage = isset($_GET['page']) ? $_GET['page'] : 1;
@@ -38,11 +29,7 @@
                 <div class="row">
                     <div class="col-1 style-col-img">
                         <a href="<?php echo site_url('Pages/guide_work_detail/' . $rs->guide_work_id); ?>">
-                            <?php if (!empty($rs->guide_work_img)) : ?>
-                                <img class="border-radius24" src="<?php echo base_url('docs/img/' . $rs->guide_work_img); ?>" width="94px" height="63px">
-                            <?php else : ?>
-                                <img class="border-radius24" src="<?php echo base_url('docs/logo.png'); ?>" width="94px" height="63px">
-                            <?php endif; ?>
+                            <img class="border-radius24" src="<?php echo base_url('docs/logo.png'); ?>" width="50px" height="50px">
                         </a>
                     </div>
                     <div class="col-9 font-pages-content">
@@ -70,10 +57,32 @@
                 </div>
             </div>
         <?php } ?>
-        <!-- แสดงปุ่ม Pagination -->
-        <div class="pagination-container d-flex justify-content-between">
+        <!-- จัดการหน้า -->
+        <div class="pagination-container d-flex justify-content-end">
             <div class="pagination-pages">
                 <ul class="pagination">
+                    <!-- ปุ่ม "กลับไปหน้าแรก" -->
+                    <?php if ($currentPage > 1) : ?>
+                        <li class="page-item pagination-item">
+                            <a class="" href="?page=1" aria-label="First">
+                                <img src="<?php echo base_url('docs/s.pages-first.png'); ?>" class="pages-first">
+                                <span aria-hidden="true"></span>
+                            </a>
+                        </li>
+                    <?php endif; ?>
+
+                    <!-- ปุ่ม Previous -->
+                    <?php if ($currentPage > 1) : ?>
+                        <li class="page-item" style="width: 55px; margin-left: -12px;">
+                            <a class="" href="?page=<?php echo $currentPage - 1; ?>" aria-label="Previous">
+                                <img src="<?php echo base_url('docs/s.pages-pre.png'); ?>" alt="Previous" class="pages-pre">
+                                <span aria-hidden="true"></span>
+                            </a>
+                        </li>
+                    <?php endif; ?>
+
+
+
                     <!-- แสดงปุ่ม "กลับไปหน้าแรก" ถ้าหน้าปัจจุบันไม่ได้ต่อเนื่องจากหน้าแรก -->
                     <?php
                     $numToShow = 3; // จำนวนปุ่มที่ต้องการแสดง
@@ -86,14 +95,19 @@
                     $endPage = min($startPage + $numToShow - 1, $totalPages);
 
                     // แสดงปุ่ม "กลับไปหน้าแรก" ถ้าหน้าปัจจุบันไม่ได้ต่อเนื่องจากหน้าแรก
-                    if ($currentPage - $numToShow > -1) {
+                    if ($startPage > 1) {
                     ?>
                         <li class="page-item pagination-item">
                             <a class="page-link" href="?page=1">1</a>
                         </li>
-                        <li class="page-item pagination-item disabled">
-                            <span class="page-link">...</span>
-                        </li>
+                        <?php if ($startPage > 2) : ?>
+                            <li class="page-item pagination-item">
+                                <a class="page-link" href="?page=2">2</a>
+                            </li>
+                            <li class="page-item pagination-item disabled">
+                                <span class="page-link">...</span>
+                            </li>
+                        <?php endif; ?>
                     <?php
                     }
 
@@ -106,11 +120,14 @@
                     <?php
                     }
 
-                    // แสดงปุ่ม "..." ถ้าหน้าไม่ได้ต่อเนื่อง
-                    if ($endPage < $totalPages) {
+                    // แสดงปุ่ม "..." ถ้าหน้าไม่ได้ต่อเนื่อง และรองสุดท้าย
+                    if ($endPage < $totalPages - 1) {
                     ?>
                         <li class="page-item pagination-item disabled">
                             <span class="page-link">...</span>
+                        </li>
+                        <li class="page-item pagination-item">
+                            <a class="page-link" href="?page=<?php echo $totalPages - 1; ?>"><?php echo $totalPages - 1; ?></a>
                         </li>
                     <?php
                     }
@@ -124,35 +141,36 @@
                     <?php
                     }
                     ?>
+                    <!-- ปุ่ม Next -->
+                    <?php if ($currentPage < $totalPages) : ?>
+                        <li class="page-item" style="width: 55px; margin-left: -10px;">
+                            <a class="" href="?page=<?php echo $currentPage + 1; ?>" aria-label="Next">
+                                <img src="<?php echo base_url('docs/s.pages-next.png'); ?>" alt="Next" class="pages-next">
+                                <span aria-hidden="true"></span>
+                            </a>
+                        </li>
+                    <?php endif; ?>
 
+                    <!-- ปุ่ม "ไปหน้าสุดท้าย" -->
+                    <?php if ($currentPage < $totalPages) : ?>
+                        <li class="page-item pagination-item">
+                            <a class="" href="?page=<?php echo $totalPages; ?>" aria-label="Last">
+                                <img src="<?php echo base_url('docs/s.pages-last.png'); ?>" alt="Last" class="pages-last">
+                                <span aria-hidden="true"></span>
+                            </a>
+                        </li>
+                    <?php endif; ?>
                 </ul>
             </div>
-            <!-- ปุ่ม Next และ Previous -->
-            <div class="pagination-next-prev row" style="list-style-type: none;">
-                <div class="col-5">
-                    <?php if ($currentPage > 1) : ?>
-                        <li class="page-item">
-                            <a class="page-link" href="?page=<?php echo $currentPage - 1; ?>" aria-label="Previous">
-                                <img src="<?php echo base_url('docs/s.pages-pre2.png'); ?>" alt="Previous" class="pagination-icon">
-                                <span aria-hidden="true"></span>
-                            </a>
-                        </li>
-                    <?php endif; ?>
-                </div>
-                <div class="col-5">
-                    <?php if ($currentPage < $totalPages) : ?>
-                        <li class="page-item">
-                            <a class="page-link" href="?page=<?php echo $currentPage + 1; ?>" aria-label="Next">
-                                <img src="<?php echo base_url('docs/s.pages-next2.png'); ?>" alt="Next" class="pagination-icon">
-                                <span aria-hidden="true"></span>
-                            </a>
-                        </li>
-                    <?php endif; ?>
-                </div>
-            </div>
         </div>
-        <div class="margin-top-delete-topic d-flex justify-content-end">
-            <a href="<?php echo site_url('Home'); ?>"><img src="<?php echo base_url("docs/s.btn-back.png"); ?>"></a>
+
+        <!-- ฟอร์มกรอกหมายเลขหน้า -->
+        <div class="pagination-jump-to-page d-flex justify-content-end">
+            <form action="" method="GET" class="d-flex">
+                <label style="font-size: 24px;">ไปหน้าที่&nbsp;&nbsp;</label>
+                <input type="number" name="page" min="1" max="<?php echo $totalPages; ?>" value="<?php echo $currentPage; ?>" class="form-control" style="width: 60px; margin-right: 10px;">
+                <input type="image" src="<?php echo base_url('docs/s.pages-go.png'); ?>" alt="Go" class="pages-go" style="width: 40px; height: 40px;">
+            </form>
         </div>
     </div>
 </div>
--- application/views/frontend/p_rpobuy_detail.php
+++ application/views/frontend/p_rpobuy_detail.php
@@ -1,80 +1,56 @@
+<div class="text-center" style="padding-top: 65px">
+    <span class="font-pages-head">รายการจัดซื้อจัดจ้างหรือจัดหาพัสดุ</span>
+</div>
+</div>
+
 <div class="bg-pages">
     <div class="container-pages-detail">
-        <div class="page-center">
-            <div class="head-pages-two">
-                <span class="font-pages-head">รายการจัดซื้อจัดจ้างหรือจัดหาพัสดุ</span>
-            </div>
+        <div class="font-pages-content-head"><?= $rsData->p_rpobuy_name; ?></div>
+        <div class="pages-content break-word mt-2">
+            <span class="font-pages-content-detail"><?= $rsData->p_rpobuy_detail; ?></span>
+            <br>
+            <a class="font-26" href="<?= $rsData->p_rpobuy_link; ?>" target="_blank"><?= $rsData->p_rpobuy_link; ?></a>
         </div>
-        <div class="row">
-            <div class="path1-1">
-                <span class="font-path-1 underline"><a href="<?php echo site_url('Home'); ?>">หน้าแรก</a></span>
-            </div>
-            <div class="path2-4">
-                <span class="font-path-2 underline"><a href="#">การจัดซื้อจัดจ้าง</a></span>
-            </div>
-        </div>
-        <div class="bg-pages-in">
-            <div class="scrollable-container-news">
-                <div class="font-pages-content-head"><?= $rsData->p_rpobuy_name; ?></div>
-                <div class="pages-content break-word mt-2">
-                    <span class="font-pages-content-detail"><?= $rsData->p_rpobuy_detail; ?></span>
-                    <br>
-                    <a class="font-26" href="<?= $rsData->p_rpobuy_link; ?>" target="_blank"><?= $rsData->p_rpobuy_link; ?></a>
-                    <?php if (!empty($rsDoc)) { ?>
-                        <span class="font-pages-content-detail">ไฟล์เอกสารเพิ่มเติม</span>
-                        <?php foreach ($rsDoc as $doc) { ?>
-                            <a class="font-doc" href="<?= base_url('docs/file/' . $doc->p_rpobuy_file_doc); ?>" target="_blank"><?= $doc->p_rpobuy_file_doc; ?></a>,&nbsp;
-                        <?php } ?>
-                    <?php } ?>
-                </div>
-                <?php foreach ($rsImg as $img) { ?>
-                    <div class="col-3 mb-3">
-                        <img class="rounded-all" src="<?php echo base_url('docs/img/' . $img->p_rpobuy_img_img); ?>" width="950px" height="100%">
-                    </div>
-                <?php } ?>
-                <?php foreach ($rsPdf as $file) { ?>
-                    <div class="row">
-                        <div class="col-6 mt-2">
-                            <div class="d-flex justify-content-start">
-                                <span class="font-page-detail-view-news">ดาวโหลดแล้ว <?= $file->p_rpobuy_pdf_download; ?> ครั้ง</span>
-                            </div>
-                        </div>
-                        <div class="col-6">
-                            <div class="d-flex justify-content-end">
-                                <a onclick="downloadFile(event, <?= $file->p_rpobuy_pdf_id; ?>)" href="<?= base_url('docs/file/' . $file->p_rpobuy_pdf_pdf); ?>" download>
-                                    <img src="<?php echo base_url("docs/s.btn-download.png"); ?>">
-                                </a>
-                                <script>
-                                    function downloadFile(event, p_rpobuy_pdf_id) {
-                                        // ทำการส่งคำร้องขอ AJAX ไปยัง URL ที่บันทึกการดาวน์โหลดพร้อมกับ ID
-                                        var xhr = new XMLHttpRequest();
-                                        xhr.open('GET', '<?= base_url('Pages/increment_download_p_rpobuy/'); ?>' + p_rpobuy_pdf_id, true);
-                                        xhr.send();
 
-                                        // ทำการเปิดไฟล์ PDF ในหน้าต่างใหม่
-                                        window.open(event.currentTarget.href, '_blank');
-                                    }
-                                </script>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="blog-text mt-3 mb-5">
-                        <object data="<?= base_url('docs/file/' . $file->p_rpobuy_pdf_pdf); ?>" type="application/pdf" width="100%" height="1500px"></object>
-                    </div>
-                <?php } ?>
+        <?php foreach ($rsImg as $img) { ?>
+            <div class="text-center">
+                <img class="rounded-all" src="<?php echo base_url('docs/img/' . $img->p_rpobuy_img_img); ?>" width="1035" height="100%">
             </div>
+            <br>
+        <?php } ?>
+
+        <?php foreach ($rsFile as $file) { ?>
             <div class="row">
-                <div class="col-6">
+                <div class="col-6 mt-2">
                     <div class="d-flex justify-content-start">
-                        <span class="font-page-detail-view-news">จำนวนผู้เข้าชม <?= $rsData->p_rpobuy_view; ?> ครั้ง</span>
+                        <span class="font-page-detail-view-news">ดาวโหลดแล้ว <?= $file->p_rpobuy_file_download; ?> ครั้ง</span>
                     </div>
                 </div>
                 <div class="col-6">
-                    <div class="margin-top-delete-topic d-flex justify-content-end">
-                        <a href="<?php echo site_url('Pages/news'); ?>"><img src="<?php echo base_url("docs/s.btn-back.png"); ?>"></a>
+                    <div class="d-flex justify-content-end">
+                        <a onclick="downloadFile(event, <?= $file->p_rpobuy_file_id; ?>)" href="<?= base_url('docs/file/' . $file->p_rpobuy_file_pdf); ?>" download>
+                            <img src="<?php echo base_url("docs/btn-download.png"); ?>" class="btn-download" >
+                        </a>
+                        <script>
+                            function downloadFile(event, p_rpobuy_file_id) {
+                                // ทำการส่งคำร้องขอ AJAX ไปยัง URL ที่บันทึกการดาวน์โหลดพร้อมกับ ID
+                                var xhr = new XMLHttpRequest();
+                                xhr.open('GET', '<?= base_url('Pages/increment_download_p_rpobuy/'); ?>' + p_rpobuy_file_id, true);
+                                xhr.send();
+
+                                // ทำการเปิดไฟล์ PDF ในหน้าต่างใหม่
+                                window.open(event.currentTarget.href, '_blank');
+                            }
+                        </script>
                     </div>
                 </div>
             </div>
+            <div class="blog-text mt-3 mb-5">
+                <object data="<?= base_url('docs/file/' . $file->p_rpobuy_file_pdf); ?>" type="application/pdf" width="100%" height="1500px"></object>
+            </div>
+        <?php } ?>
+        <div class="d-flex justify-content-start">
+            <span class="font-page-detail-view-news">จำนวนผู้เข้าชม <?= $rsData->p_rpobuy_view; ?> ครั้ง</span>
         </div>
     </div>
 </div>
